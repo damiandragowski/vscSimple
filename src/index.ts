2fe864d3d9024913bc3fe412382aa739bf76f6a2
Damian Drągowski <damian.dragowski@gmail.com>
2018-10-17 12:48:43 +0200

    union types exhaustiveness checking by using never type and cast real object to it

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,28 @@
-
-class Startup {
-    public static main(): number {
-        // console.log('Hello World');
-        return 0;
-    }
-    public SayHiToMe(name : string) {
-        return "Hi " + name;
-    }
+function assertNever(x: never): never {
+    throw new Error("Unexpected object: " + x);
 }
 
-Startup.main();
+interface A1 {
+    kind : "A1";
+    size : number;
+}
 
-export { Startup };
+interface A2 {
+    kind : "A2"
+    length : number;
+}
+
+interface A3 {
+    kind : "A3"
+    depth : number;
+}
+
+type A = A1 | A2 | A3
+
+function getDimensionSize( a : A ) {
+    switch(a.kind) {
+        case "A1": return a.size;
+        case "A2": return a.length;
+        default: assertNever(a);
+    }
+}
